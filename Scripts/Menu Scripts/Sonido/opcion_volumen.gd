2fdc08ad30184c25bbb71d05b78bfd1f55ce675b
QuimCode <quimafoncode@gmail.com>
2024-10-31 20:17:15 -0300

    Resuelto conflictos y modelos 3d de testo en Godot

--- Scripts/Menu Scripts/Sonido/opcion_volumen.gd
+++ Scripts/Menu Scripts/Sonido/opcion_volumen.gd
@@ -2,7 +2,7 @@ extends HSlider
 
 func _ready() -> void:
 	# Establece el valor del slider en función del volumen guardado
-	value = int(lerp(0, 100, (GlobalMusMenu.volume_db + 80) / 80.0))
+	value = int(lerp(0, 100, (GlobalMusMenu.volume_db + 50) / 50.0))
 
 func _on_slider_value_changed(value: int) -> void:
 	# Calcula el volumen en dB; 0 en el slider será -80 dB, y 100 será 0 dB
